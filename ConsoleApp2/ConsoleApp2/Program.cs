7f9452026b507756ff250828e6dd183761727a6a
Tareq osama <tarekaless99@gmail.com>
2017-09-13 01:35:41 +0300

    My App
    This is my test app

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("This is Manipulation Table ");
+            Console.ReadLine();
+
+            for (int i = 1; i<=10; i++) {
+
+                for (int g = 1; g<=10; g++) {
+
+
+                    Console.WriteLine(i+"x"+g+"= "+ (i*g));
+
+
+
+
+                }
+
+            }
+
+
+            Console.ReadLine();
+            
+        }
+    }
+}
